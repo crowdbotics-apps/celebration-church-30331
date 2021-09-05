b06846907e57fa38a51c1ca83da3bbaa57f3b62b
Crowdbotics <team@crowdbotics.com>
2021-09-05 18:02:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "celebration_church_30331Identifier",
+  url: "https://celebration_church_30331Identifier.botics.co",
+  api: "https://celebration_church_30331Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
